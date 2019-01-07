091b7ad4ad66d0315a2efaad02f85253ee8bf0a8
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-07 23:08:21 +0900

    optional parameter

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Danny", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} , you are a ${gender}`);
+};
+sayHi(name, age); // => error!
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+const name = "Danny",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name,age,gender?) =>{        // ? mean optional parameter
+    console.log(`Hello ${name}, you are ${age} , you are a ${gender}`)
+}
+
+sayHi(name,age);// => error!
+sayHi(name,age,gender);
+export {};
