04d8fce0ca6d08b52c4c1b8d1e2677925059b44b
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-08 21:46:54 +0900

    typestript tutorial end

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-const name = "Danny",
-    age = 24,
-    gender = "male";
-
-const sayHi = (name:string,age:number,gender:string): string =>{        // ? mean optional parameter
-    return `Hello ${name}, you are ${age} , you are a ${gender}`;
+class Human {
+    public name:string;
+    public age:number;
+    public gender:string;
+    constructor(name:string, age:number,gender:string){
+        this.name=name;
+        this.age = age;
+        this.gender = gender
+    }
 }
 
-// sayHi(name,age);// => error!
-console.log(sayHi("Danny",24,"male"));
-// sayHi(name,age,gender);
+const lynn = new Human("Lynn",18,"female");
+
+const sayHi = (person : Human): string =>{        // ? mean optional parameter
+    return `Hello ${person.name}, you are ${person.age} , you are a ${person.gender}!`;
+}
+
+console.log(sayHi(lynn));
+
 export {};
